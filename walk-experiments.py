f875017c4166b09eb06a7405e8d944a2d3c80c6c
raincoat <53009955+sffranke@users.noreply.github.com>
2024-06-20 00:05:23 +0200

    Update walk-experiments.py
    walk in every direction 0 ... -180°

--- walk-experiments.py
+++ walk-experiments.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 import time
 from math import pi
 from spot_micro_kinematics_python.utilities import spot_micro_kinematics as smk
-
 from spot_micro_kinematics_python.spot_micro_stick_figure import SpotMicroStickFigure
 
 plt.ion()
@@ -40,7 +39,7 @@ class SpotMicro:
         self.lines = self.init_lines(coords)
         self.stopwalk = False
         self.total_time = 1
-        
+
     def init_lines(self, coords):
         lines = []
         for i in range(4):
@@ -126,7 +125,7 @@ class SpotMicro:
 
     def get_current_coords(self):
         return self.kinematics.sm.get_leg_coordinates()
-        
+
     def anim(self, mypoints, pitch):
         self.kinematics.sm.set_absolute_foot_coordinates(mypoints)
         self.kinematics.sm.set_body_angles(theta=pitch * self.kinematics.d2r)
@@ -140,23 +139,6 @@ class SpotMicro:
         pitch = 5
         self.pose(moving_time, steps, angles, pitch)
 
-    def pose(self, moving_time, steps, leg_angles, pitch):
-        self.stopwalk = not self.stopwalk
-        time.sleep(1)
-        self.set_current_angles(moving_time, steps, leg_angles, pitch)
-        
-    def twist(self, roll, pitch, yaw):
-        self.kinematics.sm.set_body_angles(
-                                theta=pitch * self.kinematics.d2r,
-                                phi=roll * self.kinematics.d2r,
-                                psi=yaw * self.kinematics.d2r)
-                                
-        coords = self.get_current_coords()
-        self.update_lines(coords)
-        
-          
-    # --------- walk ---------
-    
     def calculate_start_time_offsets(self, gait):
         if gait == "wave":
             phase_time =  1 / 2
@@ -171,12 +153,11 @@ class SpotMicro:
             offsets = [0, 0, phase_time, phase_time]
             return offsets
         elif gait == "gallop":
-            # just dreaming ..self.
             phase_time = 1 / 2
             offsets = [0, 0, phase_time, phase_time]
             return offsets
-    
-    def getpositions(self, positions, name, desired_p4_points, steps, radius, start_time_offset, swing_height, swing_time_ratio):
+
+    def getpositions(self, positions, name, desired_p4_points, steps, radius, start_time_offset, swing_height, swing_time_ratio, angle=0):
         swing_time = 2 * swing_time_ratio
         stance_time = 2 * (1 - swing_time_ratio)
         swing_steps = int(steps * swing_time_ratio)
@@ -185,23 +166,29 @@ class SpotMicro:
         for t in np.linspace(0, 2, steps):
             t_mod = (t + start_time_offset) % 2
             if t_mod < stance_time / 2:
-                x, y = self.linear_motion(t_mod, stance_time / 2, 0, -radius / 2)
+                x, z = self.linear_motion(t_mod, stance_time / 2, 0, -radius / 2)
+                y = 0  # Ground level
             elif t_mod < stance_time / 2 + swing_time:
-                x, y = self.point_on_ellipse(t_mod - stance_time / 2, swing_time, radius, swing_height, reverse=True)  # Reverse direction
+                x, y = self.point_on_ellipse(t_mod - stance_time / 2, swing_time, radius, swing_height, reverse=True)
+                z = 0  # Maintain the same z position
             else:
-                x, y = self.linear_motion(t_mod - stance_time / 2 - swing_time, stance_time / 2, radius / 2, 0)
+                x, z = self.linear_motion(t_mod - stance_time / 2 - swing_time, stance_time / 2, radius / 2, 0)
+                y = 0  # Ground level
+
+            # Rotate the points by the specified angle
+            x_rot = x * np.cos(angle) - z * np.sin(angle)
+            z_rot = x * np.sin(angle) + z * np.cos(angle)
 
             if name not in positions:
                 positions[name] = []
-            positions[name].append((x, y))
-            
+            positions[name].append((x_rot, y, z_rot))
+        
         return positions
-    
- 
+
     def point_on_ellipse(self, t, T, radius, swing_height, reverse=False):
         angle = np.pi * t / T
         if reverse:
-            angle = np.pi - angle  # Reverse direction
+            angle = np.pi - angle
         x = (radius / 2) * np.cos(angle)
         y = swing_height * np.sin(angle)
         return x, y
@@ -209,27 +196,25 @@ class SpotMicro:
     def linear_motion(self, t, T, start_pos, end_pos):
         return start_pos + (end_pos - start_pos) * (t / T), 0
 
-    def walk(self, total_time, repetitions, radii, steps, gait_pattern, overlap_times, swing_heights, swing_time_ratios):
+    def walk(self, total_time, repetitions, radii, steps, gait_pattern, overlap_times, swing_heights, swing_time_ratios, angle=0):
         leg_names = ["front_right", "back_right", "back_left", "front_left"]
         
         self.total_time = total_time
         self.stepwidth = radii
         
-        start_time_offsets = self.calculate_start_time_offsets (gait_pattern)
+        start_time_offsets = self.calculate_start_time_offsets(gait_pattern)
        
         for n in range(repetitions):
             positions = {}
             for i, leg_name in enumerate(leg_names):
-                print (steps,"self.total_time: ",self.total_time)       
-                if self.stepwidth[i] < 0.08:  #limit
-                    positions = self.getpositions(positions, leg_name, self.kinematics.desired_p4_points[i], steps, self.stepwidth[i], start_time_offsets[i], swing_heights[i], swing_time_ratios[i])
+                if self.stepwidth[i] < 0.08:
+                    positions = self.getpositions(positions, leg_name, self.kinematics.desired_p4_points[i], steps, self.stepwidth[i], start_time_offsets[i], swing_heights[i], swing_time_ratios[i], angle)
                 else:
                     self.stepwidth[i] = 0.08
-                    positions = self.getpositions(positions, leg_name, self.kinematics.desired_p4_points[i], steps, self.stepwidth[i], start_time_offsets[i], swing_heights[i], swing_time_ratios[i])
+                    positions = self.getpositions(positions, leg_name, self.kinematics.desired_p4_points[i], steps, self.stepwidth[i], start_time_offsets[i], swing_heights[i], swing_time_ratios[i], angle)
                     
             if self.stopwalk:
                 break
-            print (steps,"self.stepwidth: ",self.stepwidth[i])            
             start_time = time.time()
             for step in range(steps):
                 mypoints = []
@@ -237,7 +222,7 @@ class SpotMicro:
                     p = positions[leg_name][step]
                     new_point = [self.kinematics.desired_p4_points[i][0] + p[0],
                                  self.kinematics.desired_p4_points[i][1] + p[1],
-                                 self.kinematics.desired_p4_points[i][2]]
+                                 self.kinematics.desired_p4_points[i][2] + p[2]]
                     mypoints.append(new_point)
                 
                 self.anim(mypoints, pitch=10.0)
@@ -245,110 +230,38 @@ class SpotMicro:
                 time.sleep(max(0, self.total_time / steps - elapsed))
                 start_time = time.time()
 
-    def update_speed(self, adjustment):
-        self.max = 0.1  # short time leads to more speed
-        self.min = 4
-        self.total_time += adjustment
-        if self.total_time > self.min:
-             self.total_time = 3.9
-        if self.total_time < self.max:
-             self.total_time = 0.11
-             
-       
-        if self.total_time < self.max:
-             self.total_time = 0.11
-        if self.total_time > self.min:
-             self.total_time = 3.9
-            
-        print(f"Updated total_time: {self.total_time}")
-        
-        
-    def update_stepwidth(self, adjustment):
-        self.max = 0.10
-        
-        tmp_array = []
-        for x in self.stepwidth:
-            tmp_array.append(x + adjustment)
-            self.stepwidth = tmp_array
-        print(f"Updated update_stepwidth: {self.stepwidth}")
-               
-# ###########################
-    def sideways_getpositions(self, positions, name, desired_p4_points, steps, radius, start_time_offset, swing_height, swing_time_ratio):
-        swing_time = 2 * swing_time_ratio
-        stance_time = 2 * (1 - swing_time_ratio)
-        swing_steps = int(steps * swing_time_ratio)
-        stance_steps = steps - swing_steps
-
-        for t in np.linspace(0, 2, steps):
-            t_mod = (t + start_time_offset) % 2
-            if t_mod < stance_time / 2:
-                z, y = self.linear_motion(t_mod, stance_time / 2, 0, -radius / 2)
-            elif t_mod < stance_time / 2 + swing_time:
-                z, y = self.point_on_ellipse(t_mod - stance_time / 2, swing_time, radius, swing_height, reverse=True)  # Reverse direction
-            else:
-                z, y = self.linear_motion(t_mod - stance_time / 2 - swing_time, stance_time / 2, radius / 2, 0)
-
-            if name not in positions:
-                positions[name] = []
-            positions[name].append((z, y))
-
-        return positions
-
-    def sideways_walk(self, total_time, repetitions, radii, steps, gait_pattern, overlap_times, swing_heights, swing_time_ratios, direction=1):
-        leg_names = ["front_right", "back_right", "back_left", "front_left"]
-
-        self.total_time = total_time
-        self.stepwidth = radii
-
-        start_time_offsets = self.calculate_start_time_offsets(gait_pattern)
-
-        for n in range(repetitions):
-            positions = {}
-            for i, leg_name in enumerate(leg_names):
-                print(steps, "self.total_time: ", self.total_time)
-                if self.stepwidth[i] < 0.08:  # limit
-                    positions = self.sideways_getpositions(positions, leg_name, self.kinematics.desired_p4_points[i], steps, self.stepwidth[i], start_time_offsets[i], swing_heights[i], swing_time_ratios[i])
-                else:
-                    self.stepwidth[i] = 0.08
-                    positions = self.sideways_getpositions(positions, leg_name, self.kinematics.desired_p4_points[i], steps, self.stepwidth[i], start_time_offsets[i], swing_heights[i], swing_time_ratios[i])
-
-            if self.stopwalk:
-                break
-            print(steps, "self.stepwidth: ", self.stepwidth[i])
-            start_time = time.time()
-            for step in range(steps):
-                mypoints = []
-                for i, leg_name in enumerate(leg_names):
-                    p = positions[leg_name][step]
-                    new_point = [self.kinematics.desired_p4_points[i][0],
-                                 self.kinematics.desired_p4_points[i][1] + p[1],
-                                 self.kinematics.desired_p4_points[i][2] + p[0] * direction]
-                    mypoints.append(new_point)
-
-                self.anim(mypoints, pitch=10.0)
-                elapsed = time.time() - start_time
-                time.sleep(max(0, self.total_time / steps - elapsed))
-                start_time = time.time()
-# ###########################
-
 def main():
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     walker = SpotMicro(ax)
     total_time = 2 
-    repetitions = 3
+    repetitions = 4
     steps = 30
-    radii = [0.02, 0.02, 0.02, 0.02] 
+    radii = [0.04, 0.04, 0.04, 0.04] 
     overlap_times = [0.0, 0.0, 0.0, 0.0]
     swing_heights = [0.03, 0.03, 0.03, 0.03]
     swing_time_ratios = [0.25, 0.25, 0.25, 0.25]
+    angle = np.deg2rad(0)  # example angle in radians
+    walker.walk(total_time, repetitions, radii, steps, "wave", overlap_times, swing_heights, swing_time_ratios, angle)
+    plt.pause(0.01)
     angle = np.deg2rad(45)  # example angle in radians
-    #walker.angled_walk(total_time, repetitions, radii, steps, "wave", overlap_times, swing_heights, swing_time_ratios, angle)
-    walker.sideways_walk(total_time, repetitions, radii, steps, "wave", overlap_times, swing_heights, swing_time_ratios, 1)  
-    walker.sideways_walk(total_time, repetitions, radii, steps, "wave", overlap_times, swing_heights, swing_time_ratios, -1)         
-    #walker.walk(total_time, repetitions, radii, steps, "wave", overlap_times, swing_heights, swing_time_ratios) 
-    plt.pause(0.01)        
-                               
+    walker.walk(total_time, repetitions, radii, steps, "wave", overlap_times, swing_heights, swing_time_ratios, angle)
+    plt.pause(0.01)
+    angle = np.deg2rad(-45)  # example angle in radians
+    walker.walk(total_time, repetitions, radii, steps, "wave", overlap_times, swing_heights, swing_time_ratios, angle)
+    plt.pause(0.01)
+    angle = np.deg2rad(90)  # example angle in radians
+    walker.walk(total_time, repetitions, radii, steps, "wave", overlap_times, swing_heights, swing_time_ratios, angle)
+    plt.pause(0.01)
+    plt.show()
+    angle = np.deg2rad(-90)  # example angle in radians
+    walker.walk(total_time, repetitions, radii, steps, "wave", overlap_times, swing_heights, swing_time_ratios, angle)
+    plt.pause(0.01)
+    plt.show()
+    angle = np.deg2rad(180)  # example angle in radians
+    walker.walk(total_time, repetitions, radii, steps, "wave", overlap_times, swing_heights, swing_time_ratios, angle)
+    plt.pause(0.01)
+    plt.show()
 
 if __name__ == "__main__":
     main()
