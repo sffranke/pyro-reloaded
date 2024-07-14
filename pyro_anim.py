6418a17e30cf8f2c66bc7f5bf40d6e416bbf013e
raincoat <53009955+sffranke@users.noreply.github.com>
2024-07-14 16:18:32 +0200

    Update pyro_anim.py

--- pyro_anim.py
+++ pyro_anim.py
@@ -33,7 +33,6 @@ if mode != 'angles':
     plt.ion()
 
 
-
 class StateClass:
     from enum import Enum
 
@@ -79,20 +78,21 @@ def transition_to(walker, target_state):
     walker.stopwalk = True
 
     if target_state == current_state:
-        return 
+        target_state = stateobj.set_state(stateobj.State.STAND)
         
     if current_state != stateobj.State.WALK and target_state == stateobj.State.WALK:
+    
         walker.stopwalk = True
         stateobj.set_state(stateobj.State.WALK)
-        #current_state = State.WALK
         angles = config['stand_angles']
         walker.pose(0.5, 20, angles, 5)
+        plt.pause(1)
         walker.stopwalk = False
         walker.walk(config['total_time'], config['repetitions'], config['radii'], config['steps'], "wave", config['overlap_times'], config['swing_heights'], config['swing_time_ratios'], np.deg2rad(0))
         
+
     elif target_state == stateobj.State.ROTATELEFT:
         stateobj.set_state(stateobj.State.ROTATELEFT)
-        #current_state = State.ROTATELEFT
         angles = config['stand_angles']
         walker.pose(0.5, 20, angles, 5)
         walker.stopwalk = False
@@ -100,7 +100,6 @@ def transition_to(walker, target_state):
     
     elif target_state == stateobj.State.ROTATERIGHT:
         stateobj.set_state(stateobj.State.ROTATERIGHT)
-        #current_state = State.ROTATERIGHT
         angles = config['stand_angles']
         walker.pose(0.5, 20, angles, 5)
         walker.stopwalk = False
@@ -108,25 +107,35 @@ def transition_to(walker, target_state):
 
     elif target_state == stateobj.State.REST:
         stateobj.set_state(stateobj.State.REST)
-        #current_state = State.REST
         angles = config['rest_angles']
         walker.pose(0.5, 20, angles, 5)
 
     elif target_state == stateobj.State.SIT:
         stateobj.set_state(stateobj.State.SIT)
-        #current_state = State.SIT
         angles = config['sit_angles']
         walker.pose(0.5, 20, angles, 5)
+
+    elif target_state == stateobj.State.POSE:
     
+        if walker.stopwalk == False:
+            walker.stopwalk = True
+            time.sleep(0.3)
+
+        angles = config['stand_angles']
+        walker.pose(0.5, 20, angles, 5)
+
+        time.sleep(0.5)
+        stateobj.set_state(stateobj.State.POSE)
+        
     elif target_state == stateobj.State.POOP:
+        walker.stopwalk = True
+        time.sleep(1)
         stateobj.set_state(stateobj.State.POOP)
-        #current_state = State.POOP
         angles = config['poop_angles']
         walker.pose(0.5, 20, angles, 5)
 
     elif target_state == stateobj.State.STAND:
         stateobj.set_state(stateobj.State.STAND)
-        #current_state = State.STAND
         angles = config['stand_angles']
         walker.pose(0.5, 20, angles, 5)
 
@@ -144,6 +153,7 @@ def main():
 
     walker = SpotMicro(mode, ax)
     current_state = stateobj.get_state()
+    print("current_state: ", current_state)
     rundemo = False
     runangles = False
 
@@ -175,50 +185,61 @@ def main():
         print ("           X: rest")
         print ("-----------------------------")
         print ("          up: faster")
+        print ("Left: poop         Right: pose")
         print ("        down: slower")
         print ("-----------------------------")
-        print ("           P: exit")
-        print ("    Arrow up: step faster")
-        print ("  Arrow down: step slower")
         print ("Trigger Left: step wider  Trigger Right: step narrower")
         print ("L3/R3 press: increase/decrease height")
         print ("share/options press: rotate left/right")
+        print ("           P: exit")
         
         while True:
             if not event_queue.empty():
                 event = event_queue.get()
-                #print("EVENT: ",event)
+                current_state = stateobj.get_state()
+                #print("EVENT: ",event, "State:", current_state)
                 
                 if event == "rest":
                     #print("event rest")
+                    time.sleep (0.1)
                     transition_to(walker, stateobj.State.REST)
+                    stateobj.set_state(stateobj.State.REST)
 
                 if event == "walk":
-                    #print("### event walk ----> ", current_state)
-                    #current_state = State.STAND
+                    time.sleep (0.1)
                     transition_to(walker, stateobj.State.WALK)
+                    stateobj.set_state(stateobj.State.WALK)
 
                 if event == "sit":
-                    #print("event sit")
+                    time.sleep (0.1)
                     transition_to(walker, stateobj.State.SIT)
+                    stateobj.set_state(stateobj.State.SIT)
 
                 if event == "poop":
-                    #print("event poop")
+                    time.sleep (0.1)
                     transition_to(walker, stateobj.State.POOP)
+                    stateobj.set_state(stateobj.State.POOP)
 
                 if event == "stand":
-                    #print("event stand")
+                    time.sleep (0.1)
                     transition_to(walker, stateobj.State.STAND)
+                    stateobj.set_state(stateobj.State.STAND)
                     
                 if event == "rotate_left":
-                    print("event rotate_left")
+                    time.sleep (0.1)
                     transition_to(walker, stateobj.State.ROTATELEFT)
+                    stateobj.set_state(stateobj.State.ROTATELEFT)
                     
                 if event == "rotate_right":
-                    print("event rotate_left")
+                    time.sleep (0.1)
                     transition_to(walker, stateobj.State.ROTATERIGHT)
-            if mode != 'angles':        
-                plt.pause(0.01)   
+                    stateobj.set_state(stateobj.State.ROTATERIGHT)
+
+                if event == "pose":
+                    time.sleep (0.1)
+                    transition_to(walker, stateobj.State.POSE)
+
+            plt.pause(0.01)   
 
 if __name__ == "__main__":
     main()
