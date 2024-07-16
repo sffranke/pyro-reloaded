b1ad0a28fda46b44e5220b8c6c2bfc824d7cb72b
raincoat <53009955+sffranke@users.noreply.github.com>
2024-07-16 19:07:01 +0200

    Update socketlistener.py

--- pi/socketlistener.py
+++ pi/socketlistener.py
@@ -1,5 +1,6 @@
 import socket
 import json
+import time
 import logging
 from adafruit_servokit import ServoKit
 
@@ -75,23 +76,43 @@ def control_servos(angles):
         [-1, 1, +1]  # LR
     ]
     
+    rr_coxa_pin = 3
+    rr_tibia_pin = 5
+    rr_femur_pin = 6
+    
+    rf_coxa_pin = 8
+    rf_tibia_pin = 9
+    rf_femur_pin = 10
+    
+    lf_coxa_pin = 0
+    lf_tibia_pin = 4
+    lf_femur_pin = 15
+    
+    lr_coxa_pin = 12
+    lr_tibia_pin = 13
+    lr_femur_pin = 14
+    
     servo_angles = [
-        (3, 90 + rotations[0][0] * (angle[0][0] + corrections[0][0])),  # RR coxa
-        (5, 90 + rotations[0][1] * (angle[0][1] + corrections[0][1])),  # RR tibia
-        (6, 180 - (angle[0][2] - corrections[0][2])),  # RR femur
-        (8, 90 + rotations[1][0] * (angle[1][0] + corrections[1][0])),  # RF coxa
-        (9, 90 + rotations[1][1] * (angle[1][1] + corrections[1][1])),  # RF tibia
-        (10, 180 - (angle[1][2] - corrections[1][2])),  # RF femur
-        (0, 90 + rotations[2][0] * (angle[2][0] + corrections[2][0])),  # LF coxa
-        (1, 90 + rotations[2][1] * (angle[2][1] + corrections[2][1])),  # LF tibia
-        (2, 180 - (angle[2][2] - corrections[2][2])),  # LF femur
-        (12, 90 + rotations[3][0] * (angle[3][0] + corrections[3][0])),  # LR coxa
-        (13, 90 + rotations[3][1] * (angle[3][1] + corrections[3][1])),  # LR tibia
-        (14, angle[3][2] + corrections[3][2])  # LR femur
+        (rr_coxa_pin, 90 + rotations[0][0] * (angle[0][0] + corrections[0][0])),  # RR coxa
+        (rr_tibia_pin, 90 + rotations[0][1] * (angle[0][1] + corrections[0][1])),  # RR tibia
+        (rr_femur_pin, 180 - (angle[0][2] - corrections[0][2])),  # RR femur
+        
+        (rf_coxa_pin, 90 + rotations[1][0] * (angle[1][0] + corrections[1][0])),  # RF coxa
+        (rf_tibia_pin, 90 + rotations[1][1] * (angle[1][1] + corrections[1][1])),  # RF tibia
+        (rf_femur_pin, 180 - (angle[1][2] - corrections[1][2])),  # RF femur
+        
+        (lf_coxa_pin, 90 + rotations[2][0] * (angle[2][0] + corrections[2][0])),  # LF coxa
+        (lf_tibia_pin, 90 + rotations[2][1] * (angle[2][1] + corrections[2][1])),  # LF tibia
+        (lf_femur_pin, 180 - (angle[2][2] - corrections[2][2])),  # LF femur
+        
+        (lr_coxa_pin, 90 + rotations[3][0] * (angle[3][0] + corrections[3][0])),  # LR coxa
+        (lr_tibia_pin, 90 + rotations[3][1] * (angle[3][1] + corrections[3][1])),  # LR tibia
+        (lr_femur_pin, angle[3][2] + corrections[3][2])  # LR femur
     ]
 
     for servo, angle in servo_angles:
         kit.servo[servo].angle = angle
+        #time.sleep(0.0001)
         #logging.info(f"Set servo {servo} to angle {angle}")
 
 if __name__ == "__main__":
